f5867f0d9d21b9560f3aa515ddfe840e755f59f2
Smith <greatsimonsmith@gmail.com>
2017-12-27 17:00:56 +0100

    Creating a project setup
    Using the jar file for LibGDX 1.9.8 project setup with Desktop, Android, Ios, Html as sub projects.

---
+++ desktop/src/se/smiths/gdxexample/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package se.smiths.gdxexample.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import se.smiths.gdxexample.GdxExample;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new GdxExample(), config);
+	}
+}
